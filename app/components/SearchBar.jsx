505f586b8d788456a347fb51863c4f5c88fedd95
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-03 17:48:53 +0530

    Fix padding on search bar

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -4,12 +4,15 @@ import { ChevronRightIcon } from "@heroicons/react/24/solid";
 const SearchBar = () => {
   return (
     <div className="flex w-[90%] overflow-hidden rounded-xl bg-white lg:w-[768px]">
-      <label className="sr-only">Search for IP address</label>
+      <label htmlFor="ipAddress" className="sr-only">
+        Search for IP address
+      </label>
       <input
         type="text"
         id="ipAddress"
+        maxLength="15"
         placeholder="Search..."
-        className="focus mx-2 flex-1 text-xl focus:outline-none"
+        className="focus mx-2 flex-1 pl-2 text-xl focus:outline-none"
       />
       <button type="submit" className="bg-black p-4">
         <ChevronRightIcon className="h-5 w-5 text-white" />
