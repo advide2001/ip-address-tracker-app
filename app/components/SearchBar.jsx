e6ded5523054197c829da4584b02e3c8fba9243e
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-03 17:37:02 +0530

    Build and styled the SearchBar omponent

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="flex w-[90%] overflow-hidden rounded-xl bg-white lg:w-[768px]">
+      <label className="sr-only">Search for IP address</label>
+      <input
+        type="text"
+        id="ipAddress"
+        placeholder="Search..."
+        className="focus mx-2 flex-1 text-xl focus:outline-none"
+      />
+      <button type="submit" className="bg-black p-4">
+        <ChevronRightIcon className="h-5 w-5 text-white" />
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
