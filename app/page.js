972934767608ef565eb43b5997cc7aad342a19fa
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-03 16:14:59 +0530

    Moved Hero component

--- app/page.js
+++ app/page.js
@@ -5,14 +5,15 @@ import DataCard from "./components/DataCard";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="h-[35vh] w-full bg-hero-mobileBg bg-cover bg-center bg-no-repeat md:h-[25vh] md:bg-hero-desktopBg"></div>
-      <Map />
-      <div className="fixed inset-0 z-10 flex flex-col">
-        <h1>IP Address Tracker</h1>
+    <div>
+      <div className="h-[35vh] w-full bg-hero-mobileBg bg-cover bg-center bg-no-repeat md:h-[30vh] md:bg-hero-desktopBg">
+        <h1 className="text-3xl font-semibold text-white">
+          IP Address Tracker
+        </h1>
         <SearchBar />
         <DataCard />
       </div>
+      <Map />
     </div>
   );
 }
