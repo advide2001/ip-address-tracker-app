029d6cad1c9321b56c920a7c1dad0b6e214caa5a
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-04 12:02:52 +0530

    Make DataCard component responsive

--- app/components/DataCard.jsx
+++ app/components/DataCard.jsx
@@ -9,30 +9,32 @@ const data = {
 
 const DataCard = () => {
   return (
-    <div className="absolute bottom-0 left-0 z-10 mx-auto flex w-[90%] translate-x-[5%] translate-y-1/2 flex-col items-center justify-evenly gap-4 rounded-xl bg-white px-6 py-4">
+    <div className="absolute bottom-0 left-0 z-10 mx-auto flex w-[90%] translate-x-[5%] translate-y-1/2 flex-col items-center justify-evenly gap-4 rounded-xl bg-white px-6 py-4 md:left-1/2 md:w-[70%] md:-translate-x-1/2 md:flex-row">
       <div className="mx-4 text-center">
         <p className="text-xs font-semibold uppercase tracking-wide text-gray-500">
           IP Address
         </p>
-        <p className="mt-1 text-lg font-semibold">{data.ipAddress}</p>
+        <p className="mt-1 text-lg font-semibold lg:text-xl">
+          {data.ipAddress}
+        </p>
       </div>
       <div className="mx-4 text-center">
         <p className="text-xs font-semibold uppercase tracking-wide text-gray-500">
           Location
         </p>
-        <p className="mt-1 text-lg font-semibold">{data.location}</p>
+        <p className="mt-1 text-lg font-semibold lg:text-xl">{data.location}</p>
       </div>
       <div className="mx-4 text-center">
         <p className="text-xs font-semibold uppercase tracking-wide text-gray-500">
           Timezone
         </p>
-        <p className="mt-1 text-lg font-semibold">{data.timezone}</p>
+        <p className="mt-1 text-lg font-semibold lg:text-xl">{data.timezone}</p>
       </div>
       <div className="mx-4 text-center">
         <p className="text-xs font-semibold uppercase tracking-wider text-gray-500">
           ISP
         </p>
-        <p className="mt-1 text-lg font-semibold">{data.isp}</p>
+        <p className="mt-1 text-lg font-semibold lg:text-xl">{data.isp}</p>
       </div>
     </div>
   );
