9cb7f5c33b86a725ff884c2f06525c8c69c73cb4
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-19 18:30:53 +0530

    fetch initial data on the server side

--- app/page.js
+++ app/page.js
@@ -2,18 +2,28 @@ import React from "react";
 import Map from "./components/Map";
 import SearchBar from "./components/SearchBar";
 import DataCard from "./components/DataCard";
+import fetchIPData from "./util/getIPAddressDetails";
 
-export default function Home() {
+export default async function Home() {
+  const intitalData = await fetchIPData("216.58.214.46");
+  const coords = [intitalData.lat, intitalData.lon];
+  const details = {
+    ip: intitalData.query,
+    location: `${intitalData.city}, ${intitalData.region} ${intitalData.zip}`,
+    timezone: intitalData.timezone,
+    isp: intitalData.isp,
+  };
+
   return (
     <div>
       <div className="relative flex h-[35vh] w-full flex-col justify-start gap-8 bg-hero-mobileBg bg-cover bg-center bg-no-repeat text-center md:h-[30vh] md:bg-hero-desktopBg">
         <h1 className="pt-6 text-3xl font-medium text-white">
           IP Address Tracker
         </h1>
-        <SearchBar />
-        <DataCard />
+        <SearchBar initialData={intitalData} />
+        <DataCard ipDetails={details} />
       </div>
-      <Map />
+      <Map coords={coords} />
     </div>
   );
 }
