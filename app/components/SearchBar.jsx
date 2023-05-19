b1fb2b2a8d7ed8d67dc9a3e73ed4f9c86a5fcfbd
Adarsh Desai <desaiadarsh22@gmail.com>
2023-05-19 11:47:20 +0530

    Added event handlers to handle events on SearchBar component

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,24 +1,51 @@
+"use client";
+
 import React from "react";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const SearchBar = () => {
+  // logic
+  const [ipAddress, setIpAddress] = useState("");
+  const [isValidIP, setIsValidIP] = useState(false);
+
+  const handleInputChange = (e) => {
+    setIpAddress(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ipAddress);
+    setIpAddress("");
+  };
+
+  // render output
   return (
-    <div className="mx-auto flex w-[90%] overflow-hidden rounded-xl bg-white lg:w-[768px]">
-      <label htmlFor="ipAddress" className="sr-only">
-        Search for IP address
-      </label>
-      <input
-        type="text"
-        id="ipAddress"
-        size={12}
-        maxLength={15}
-        placeholder="Search..."
-        className="flex-1 pl-3 text-xl focus:outline-none"
-      />
-      <button type="submit" className="bg-black p-4">
-        <ChevronRightIcon className="h-5 w-5 text-white" />
-      </button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="mx-auto flex w-[90%] overflow-hidden rounded-xl bg-white lg:w-[768px]">
+        <label htmlFor="ipAddress" className="sr-only">
+          Search for IP address
+        </label>
+        <input
+          type="text"
+          id="ipAddress"
+          size={12}
+          maxLength={15}
+          placeholder="Search..."
+          className="flex-1 pl-3 text-xl focus:outline-none"
+          onChange={handleInputChange}
+          value={ipAddress}
+        />
+        <button type="submit" className="bg-black p-4">
+          <ChevronRightIcon className="h-5 w-5 text-white" />
+        </button>
+      </div>
+      {isValidIP && (
+        <p className="mx-auto flex w-[90%] text-lg text-red-600 lg:w-[768px]">
+          * Enter a valid IP address
+        </p>
+      )}
+    </form>
   );
 };
 
